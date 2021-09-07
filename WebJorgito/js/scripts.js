454f4c504a3c2756377b987352dd4fca47aa8b74
Adriroca20 <hardhe21@gmail.com>
2021-09-07 12:24:51 +0200

    aboutMe

---
+++ WebJorgito/js/scripts.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: ["Diseñador 3D", "Animador","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Diseñador 3D", "Animador","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
