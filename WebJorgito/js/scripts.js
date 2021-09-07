75533a825961f153cbff18dd9794b16157449204
adriroca20 <78817869+adriroca20@users.noreply.github.com>
2021-09-07 20:46:55 +0200

    Web Casi acabada

--- WebJorgito/js/scripts.js
+++ WebJorgito/js/scripts.js
@@ -11,3 +11,10 @@ var typed = new Typed(".typing-2", {
     backSpeed: 60,
     loop: true
 });
+
+var typed2 = new Typed(".typing-3", {
+    strings: ["Volleyball", "XXX","XXX"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
