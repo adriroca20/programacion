edb49469c2483478cef98a831e5a9dc02dbd6f63
adriroca20 <78817869+adriroca20@users.noreply.github.com>
2021-09-09 23:31:29 +0200

    Add files via upload

--- WebJorgito/js/scripts.js
+++ WebJorgito/js/scripts.js
@@ -1,14 +1,14 @@
 var typed = new Typed(".typing", {
-    strings: ["Diseñador 3D", "Animador","Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
+    strings: ["Diseño 3D", "Integración de videojuegos"],
+    typeSpeed: 70,
+    backSpeed: 40,
     loop: true
 });
 
 var typed = new Typed(".typing-2", {
-    strings: ["Volleyball", "XXX","XXX"],
-    typeSpeed: 100,
-    backSpeed: 60,
+    strings: ["Modelado 3D", "Texturizado 3D","UVs", "Animación 3D", "Ilustración", "Renderizado 3D"],
+    typeSpeed: 40,
+    backSpeed: 20,
     loop: true
 });
 
