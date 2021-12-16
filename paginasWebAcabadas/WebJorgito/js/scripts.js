534ab40b676d59c8b1db45b9879f8a329514c951
adriroca20 <hardhe21@gmail.com>
2021-12-16 20:14:43 +0100

    Redistribucion del repo

---
+++ paginasWebAcabadas/WebJorgito/js/scripts.js
@@ -0,0 +1,23 @@
+var typed = new Typed(".typing", {
+    strings: ["Diseño 3D", "Integración de videojuegos"],
+    typeSpeed: 70,
+    backSpeed: 40,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Modelado 3D", "Texturizado 3D","UVs", "Animación 3D", "Ilustración", "Renderizado 3D"],
+    typeSpeed: 40,
+    backSpeed: 20,
+    loop: true
+});
+
+var typed2 = new Typed(".typing-3", {
+    strings: ["Volleyball", "XXX","XXX"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+
