f7c6387e0d8e40c58b5c1448d01e65e7bf40dee9
adriroca20 <hardhe21@gmail.com>
2022-04-12 00:01:08 +0200

    Inicio proyecto React

---
+++ tutoriales/React-Typescript/react-app-with-typescript/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [number, setNumber] = useState(5);
+
+  const changeNumber =()=> {
+    setNumber("2");
+  }
+  return (
+    <div className="App">
+    {number}
+    <button onClick={changeNumber}> Change number</button>
+    </div>
+  );
+}
+
+export default App;
