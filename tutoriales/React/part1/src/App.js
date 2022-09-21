1622bc7daa978d12df39755f5f8f7d8170fb3022
adriroca20 <hardhe21@gmail.com>
2022-09-21 23:18:02 +0200

    Imagen portfolio

---
+++ tutoriales/React/part1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Mensaje from './mensaje.js';
+
+function App() {
+  return (
+  <div className='App'>
+    <Mensaje msg="Hola"/>
+    <Mensaje msg="esto es"/>
+    <Mensaje msg="una prueba"/>
+  </div>
+  )
+}
+
+export default App;
---
+++ tutoriales/React/part1/src/mensaje.js
@@ -0,0 +1,8 @@
+
+//Aqui se llaman props
+
+function Mensaje(atributo){
+    return <h1>{atributo.msg}</h1>
+}
+
+export default Mensaje;
